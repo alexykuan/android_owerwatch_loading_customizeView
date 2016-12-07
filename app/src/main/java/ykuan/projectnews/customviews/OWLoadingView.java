0111246666f2b4d13b3aefa5744be82ad07cf715
alexkk <975934847@qq.com>
2016-12-08 02:10:52 +0800

    yeah~

---
+++ app/src/main/java/ykuan/projectnews/customviews/OWLoadingView.java
@@ -0,0 +1,149 @@
+package ykuan.projectnews.customviews;
+
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.os.Handler;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.ScaleAnimation;
+import android.widget.LinearLayout;
+import ykuan.projectnews.R;
+
+public class OWLoadingView extends ViewGroup {
+    private int [] left_p =new int[7];
+    private int [] top_p =new int[7];
+    private int [] right_p =new int[7];
+    private int [] bottom_p =new int[7];
+    private int currentViewId = 0 ;
+    private boolean At_Once = true ;
+    private Boolean flag_Increase = true ;
+    private Handler addHandler,removeHandler;
+    private OWchildView[] oWchildViews;
+    private int side_length;
+    public OWLoadingView(Context context) {
+        super(context);
+
+    }
+
+    public OWLoadingView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        oWchildViews = new OWchildView[7];
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                LayoutParams.WRAP_CONTENT,LayoutParams.WRAP_CONTENT);
+        for(int i=0;i<7;i++) {
+            oWchildViews[i]= new OWchildView(context,attrs);
+            oWchildViews[i].setImageResource(R.drawable.img1);
+            oWchildViews[i].setLayoutParams(params);
+            addView(oWchildViews[i]);
+        }
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        View[] views = new View[getChildCount()];
+//        测算单个view的正六边形边长
+        OWchildView oWchildView = (OWchildView) getChildAt(0);
+        side_length = oWchildView.getSide_Length();
+//        初始化7个view坐标
+        int size1p5 = (int) Math.ceil(1.5*side_length);
+        int sizeqrt = (int) Math.ceil(Math.sqrt(3)*side_length/2);
+        left_p = new int[]{size1p5,3*side_length,3*side_length,
+                size1p5,0,0,size1p5};
+        top_p = new int[]{0,sizeqrt,3*sizeqrt,4*sizeqrt,3*sizeqrt,sizeqrt,2*sizeqrt};
+        right_p =new int[]{2*side_length+size1p5,5*side_length,5*side_length,
+                2*side_length+size1p5,2*side_length,2*side_length,2*side_length+size1p5};
+        bottom_p= new int[]{2*sizeqrt,3*sizeqrt,5*sizeqrt,6*sizeqrt,5*sizeqrt,
+                3*sizeqrt,4*sizeqrt};
+        for(int i= 0 ;i<getChildCount();i++){
+            views[i] =getChildAt(i);
+            measureChild(views[i],widthMeasureSpec,heightMeasureSpec);
+            ViewInfo info = new ViewInfo(
+                    left_p[i],top_p[i],right_p[i],bottom_p[i]);
+            views[i].setTag(info);
+        }
+
+        setMeasuredDimension(5*side_length,
+                (int) Math.ceil(Math.sqrt(3)*3*side_length));
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, final int r, int b) {
+        /*View[] views = new View[getChildCount()];
+        for(int i=0;i<getChildCount();i++){
+            views[i] =getChildAt(i);
+            views[i].setVisibility(INVISIBLE);
+            ViewInfo info = (ViewInfo) views[i].getTag();
+            views[i].layout(info.l,info.t,info.r,info.b);
+        }*/
+        if(At_Once){
+            addHandler = new Handler();
+            removeHandler = new Handler();
+            addHandler.postDelayed(addRunnable,200);
+            At_Once = false ;
+        }
+    }
+
+
+    public class ViewInfo{
+        private int l,r,t,b;
+        public ViewInfo(int l,int t,int r,int b){
+            this.l = l;
+            this.t = t;
+            this.r = r;
+            this.b = b;
+        }
+    }
+    public void removeChildView(int position){
+        getChildAt(position).setVisibility(INVISIBLE);
+        ScaleAnimation animation = new ScaleAnimation(1,0.5f,1,0.5f,
+                Animation.RELATIVE_TO_SELF,0.5f, Animation.RELATIVE_TO_SELF,0.5f);
+        animation.setDuration(200);
+        getChildAt(position).startAnimation(animation);
+    }
+    public void addChildView(int position){
+        getChildAt(position).setVisibility(VISIBLE);
+        ViewInfo info = (ViewInfo) getChildAt(position).getTag();
+        getChildAt(position).layout(info.l,info.t,info.r,info.b);
+        ScaleAnimation animation = new ScaleAnimation(0.5f,1,0.5f,1,
+                Animation.RELATIVE_TO_SELF,0.5f, Animation.RELATIVE_TO_SELF,0.5f);
+        animation.setDuration(200);
+        getChildAt(position).startAnimation(animation);
+        //invalidate();
+    }
+    public Runnable addRunnable = new Runnable() {
+        @Override
+        public void run() {
+            addChildView(currentViewId);
+            currentViewId++;
+            if(currentViewId == 7){
+                currentViewId = 6;
+                addHandler.removeCallbacks(addRunnable);
+                removeHandler.postDelayed(removeRunnable,200);
+            }
+            else{
+                addHandler.postDelayed(addRunnable,200);
+            }
+        }
+    };
+
+    public Runnable removeRunnable =new Runnable() {
+        @Override
+        public void run() {
+            removeChildView(6-currentViewId);
+            currentViewId--;
+            if(currentViewId == -1){
+                currentViewId = 0 ;
+                removeHandler.removeCallbacks(removeRunnable);
+                addHandler.postDelayed(addRunnable,200);
+            }else{
+                removeHandler.postDelayed(removeRunnable,200);
+            }
+        }
+    };
+
+}
---
+++ app/src/main/java/ykuan/projectnews/customviews/OWchildView.java
@@ -0,0 +1,111 @@
+package ykuan.projectnews.customviews;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Bitmap;
+import android.graphics.BitmapShader;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
+import android.graphics.Path;
+import android.graphics.Shader;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.widget.ImageView;
+import android.graphics.Paint;
+
+import ykuan.projectnews.R;
+
+
+public class OWchildView extends ImageView {
+    private static int DEFAULT_SIDE_LENGTH = 40 ;
+    private int SIDE_LENGTH;
+    private Paint mPaint;
+    public OWchildView(Context context) {
+        super(context,null);
+    }
+
+    public OWchildView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        TypedArray array = context.getResources().obtainAttributes(attrs,R.styleable.OWchildView);
+        SIDE_LENGTH =array.getInt(R.styleable.OWchildView_side_length,DEFAULT_SIDE_LENGTH);
+        array.recycle();
+    }
+
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int widthMode = MeasureSpec.getMode(widthMeasureSpec);
+        int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+        int sizeWidth = MeasureSpec.getSize(widthMeasureSpec);
+        int sizeHeight = MeasureSpec.getSize(heightMeasureSpec);
+        int viewWidth = 2 * SIDE_LENGTH;
+        int viewHeight = (int) Math.ceil (Math.sqrt(3) * 1.0f *SIDE_LENGTH);
+        setMeasuredDimension(viewWidth,viewHeight);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        if(getDrawable() == null){
+            return;
+        }else {
+            setUpShader();
+            Path mPath = new Path();
+            float height = (float) (Math.sqrt(3)*SIDE_LENGTH);
+            mPath.moveTo(SIDE_LENGTH/2,0);
+            mPath.lineTo(0,height/2);
+            mPath.lineTo(SIDE_LENGTH/2,height);
+            mPath.lineTo((float) (SIDE_LENGTH*1.5),height);
+            mPath.lineTo(2*SIDE_LENGTH,height/2);
+            mPath.lineTo((float) (SIDE_LENGTH*1.5),0);
+            mPath.lineTo(SIDE_LENGTH/2,0);
+            mPath.close();
+            canvas.drawPath(mPath,mPaint);
+        }
+    }
+    public void setUpShader(){
+        Matrix matrix = new Matrix();
+        Drawable drawable = getDrawable();
+        if (drawable == null)
+        {
+            return;
+        }
+        Bitmap bmp = drawableToBitmap(drawable);
+        // 将bmp作为着色器，就是在指定区域内绘制bmp
+        BitmapShader mBitmapShader = new BitmapShader(bmp, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP);
+        float scaleHeight ;
+        float scaleWidth ;
+        scaleWidth =  getWidth() * 1.0f / bmp.getWidth();
+        scaleHeight = getHeight() * 1.0f / bmp.getHeight() ;
+        matrix.setScale(scaleWidth,scaleHeight);
+        mBitmapShader.setLocalMatrix(matrix);
+        mPaint =new Paint();
+        mPaint.setShader(mBitmapShader);
+    }
+    public Bitmap drawableToBitmap(Drawable drawable){
+        if(getDrawable() instanceof BitmapDrawable)
+        {
+            BitmapDrawable bd = (BitmapDrawable) drawable;
+            return bd.getBitmap();}
+        else{
+            int w = drawable.getIntrinsicWidth();
+            int h = drawable.getIntrinsicHeight();
+            Bitmap bitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+            Canvas canvas = new Canvas(bitmap);
+            drawable.draw(canvas);
+            return bitmap;
+        }
+    }
+
+    public void setSide_Length(int length){
+        if(length >= 0){
+            this.SIDE_LENGTH = length;
+        }else{
+            this.SIDE_LENGTH = DEFAULT_SIDE_LENGTH ;
+        }
+    }
+    public int getSide_Length(){
+        return this.SIDE_LENGTH;
+    }
+}
